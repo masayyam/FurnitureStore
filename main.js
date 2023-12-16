9d3492753a50da5894649340538f3946bca55436
masayyam <msayam055@gmail.com>
2023-12-16 11:48:28 +0500

    minor changes

---
+++ main.js
@@ -0,0 +1,5 @@
+let fname = document.getElementById("fname");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let btnSignIn = document.getElementById("btnsignin");
+let btnSignUp = document.getElementById("btnsignup");
