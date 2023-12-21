dd4a051c8f1bb5d0a85073e23bce0e59720fe6ac
masayyam <msayam055@gmail.com>
2023-12-21 13:12:51 +0500

    registation form and validation

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-let fname = document.getElementById("fname");
-let email = document.getElementById("email");
-let password = document.getElementById("password");
-let btnSignIn = document.getElementById("btnsignin");
-let btnSignUp = document.getElementById("btnsignup");
+let form = document.getElementById("reg-form");
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  let firstName = document.getElementById("rfname").value;
+  let lastName = document.getElementById("rlname").value;
+  let email = document.getElementById("remail").value;
+  let password = document.getElementById("rpassword").value;
+  if (
+    firstName.trim() === "" ||
+    lastName.trim() === "" ||
+    email.trim() === "" ||
+    password.trim() === ""
+  ) {
+    alert("Please fill out all the fields first");
+  } else {
+    alert(
+      `first name is ${firstName}, Last name is ${lastName}, Email is ${email}`
+    );
+  }
+});
